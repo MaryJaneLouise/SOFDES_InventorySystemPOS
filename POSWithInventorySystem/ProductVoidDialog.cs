b9a329a91373c4115f008d18efc61bc4a0ff22d8
MaryJaneLouise <89403991+MaryJaneLouise@users.noreply.github.com>
2022-06-29 03:42:16 -0400

    Update ProductVoidDialog.cs

--- POSWithInventorySystem/ProductVoidDialog.cs
+++ POSWithInventorySystem/ProductVoidDialog.cs
@@ -9,62 +9,49 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace POSWithInventorySystem
-{
-    public partial class ProductVoidDialog : Form
-    {
-        public ProductVoidDialog()
-        {
-            InitializeComponent();
-        }
+namespace POSWithInventorySystem {
+    public partial class ProductVoidDialog : Form {
         //string connectionString = @"Server=localhost;Database=posinventorysystem;Uid=root;Pwd=admin;SslMode=none";
         string connectionString = DatabaseConnection.Connection;
-
-        private void ProductVoidDialog_Load(object sender, EventArgs e)
-        {
+        
+        public ProductVoidDialog() {
+            InitializeComponent();
+        }
+        
+        private void ProductVoidDialog_Load(object sender, EventArgs e) {
             txtPassword.isPassword = true;
         }
 
-        private void ProductVoidDialog_KeyDown(object sender, KeyEventArgs e)
-        {
-            if(e.KeyCode == Keys.Enter)
-            {
-                //If true
-                if (ValidateAdminPassword())
-                {
+        private void ProductVoidDialog_KeyDown(object sender, KeyEventArgs e) {
+            if(e.KeyCode == Keys.Enter) {
+                if (ValidateAdminPassword()) {
                     POSTransactionForm pOS = (POSTransactionForm)this.Owner;
                     pOS.result = true;
                     this.Close();
                 }          
             }
-            if(e.KeyCode == Keys.Escape)
-            {
+            
+            if(e.KeyCode == Keys.Escape) {
                 this.Close();
             }
         }
 
-        private void btnOk_Click(object sender, EventArgs e)
-        {
-            //If true
-            if (ValidateAdminPassword())
-            {
+        private void btnOk_Click(object sender, EventArgs e) {
+            if (ValidateAdminPassword()) {
                 POSTransactionForm pOS = (POSTransactionForm)this.Owner;
                 pOS.result = true;
                 this.Close();
             }
         }
 
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
+        private void btnCancel_Click(object sender, EventArgs e) {
             this.Close();
         }
 
-        private bool ValidateAdminPassword()
-        {
+        private bool ValidateAdminPassword() {
             bool result = false;
 
-            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
-            {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString)) {
                 mysqlCon.Open();
                 MySqlCommand mySqlCommand = new MySqlCommand("SelectAdminPassword", mysqlCon);
                 mySqlCommand.CommandType = CommandType.StoredProcedure;              
@@ -73,30 +60,27 @@ namespace POSWithInventorySystem
                 DataTable dt = new DataTable();
                 dt.Load(dr);
                 
-                if(dt.Rows.Count >= 1)
-                {
+                if(dt.Rows.Count >= 1) {
                    result = true;
                 }
-                else
-                {
-                    MessageBox.Show("Invalid Password", "Incorrect", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+                else {
+                    MessageBox.Show("You've entered the wrong admin's password. Please try again.", "Incorrect Password", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
 
             return result;
         }
 
-        private string Hash(string password)
-        {
+        private string Hash(string password) {
             var bytes = new UTF8Encoding().GetBytes(password);
             byte[] hashBytes;
-            using (var algorithm = new System.Security.Cryptography.SHA512Managed())
-            {
+            
+            using (var algorithm = new System.Security.Cryptography.SHA512Managed()) {
                 hashBytes = algorithm.ComputeHash(bytes);
             }
 
             return Convert.ToBase64String(hashBytes);
         }
-
     }
 }
